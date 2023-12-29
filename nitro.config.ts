bea744b9a28fae18e548b97c5945f3471eea0639
Jazee6 <jmzzzcn@gmail.com>
2023-12-29 15:23:29 +0800

    V0.1 Test

---
+++ nitro.config.ts
@@ -0,0 +1,12 @@
+//https://nitro.unjs.io/config
+export default defineNitroConfig({
+    preset: 'vercel_edge',
+    vercel: {
+        regions: ["cle1", "iad1", "pdx1", "sfo1", "sin1", "syd1", "hnd1", "kix1"]
+    },
+    routeRules: {
+        '/**': {
+            proxy: 'https://generativelanguage.googleapis.com/**'
+        }
+    }
+})
