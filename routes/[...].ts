4151b868cff8f891689f99372275cae48041baf7
Jazee6 <jmzzzcn@gmail.com>
2024-03-27 22:09:49 +0800

    V0.2 stream

--- routes/[...].ts
+++ routes/[...].ts
@@ -3,14 +3,11 @@ const PROXY_URL = 'https://generativelanguage.googleapis.com'
 export default defineEventHandler(async event => {
     const url = getRequestURL(event)
     const headers = getRequestHeaders(event)
-    let body = null
-    if (event.method === 'POST') {
-        body = await readBody(event)
-    }
+    const body = await readBody(event)
     const res = await fetch(PROXY_URL + url.pathname + url.search, {
         method: event.method,
         headers: headers,
-        body: body,
+        body,
     })
     return new Response(res.body, {
         status: res.status,
