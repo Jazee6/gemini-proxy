2825f8aaf66a62c9a5bab334b269fb2cb2e45be4
Jazee6 <jmzzzcn@gmail.com>
2024-06-04 12:30:06 +0800

    fix: regions

--- nitro.config.ts
+++ nitro.config.ts
@@ -2,7 +2,7 @@
 export default defineNitroConfig({
     preset: 'vercel_edge',
     vercel: {
-        regions: ["cle1", "iad1", "pdx1", "sfo1", "sin1", "syd1", "hnd1", "kix1"]
+        regions: ["hkg1", "sin1", "syd1"]
     },
     // routeRules: {
     //     '/**': {
